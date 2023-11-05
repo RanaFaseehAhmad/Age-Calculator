8972e0b78f95435c281d835ed8efb720be8d8a93
RanaFaseehAhmad <149906963+RanaFaseehAhmad@users.noreply.github.com>
2023-11-06 03:12:40 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let button = document.querySelector(".btn");
+let inputfield = document.getElementById("inputfield")
+let result = document.getElementById("result")
+button.addEventListener("click", () => {
+    let birthdate = new Date(inputfield.value)
+    let bday = birthdate.getDate();
+    let bmonth = birthdate.getMonth() + 1;
+    let byear = birthdate.getFullYear();
+    let today = new Date();
+    let tday = today.getDate();
+    let tmonth = today.getMonth() + 1;
+    let tyear = today.getFullYear();
+    let ageMonth, agedate, ageyear;
+    ageyear = tyear - byear;
+    if (tmonth >= bmonth) {
+
+        ageMonth = tmonth - bmonth;
+    } else {
+        ageyear--;
+        ageMonth = 12 + tmonth - bmonth;
+    }
+    if (tday >= bday) {
+
+        agedate = tday - bday;
+
+    }
+    result.innerHTML = `you are ${ageyear} years ${ageMonth} Months and ${agedate} Old`;
+    // else {
+    //     ageMonth--;
+    //     agedate = (30.45 * tmonth + tday) - (30.45 * bmonth + bday);
+    //     agedate = (30.42 * bmonth + tday) - 365;
+    // }
+    console.log("You are  :" + ageyear + "Year," + ageMonth + " months and " + agedate + "Days Old :")
+}
+);
